f7fa02cefbe795c147160cdd8e1229388e785eed
todstef <84103756+todstef@users.noreply.github.com>
2022-07-02 17:22:24 +0200

    Add files via upload

---
+++ GameApplication/GameApplication/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace GameApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.gameMessage = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnPlayGame = new System.Windows.Forms.Button();
+            this.pictureBoxCar = new System.Windows.Forms.PictureBox();
+            this.bullet = new System.Windows.Forms.PictureBox();
+            this.policeOfficer = new System.Windows.Forms.PictureBox();
+            this.burglar2 = new System.Windows.Forms.PictureBox();
+            this.ordinaryMan = new System.Windows.Forms.PictureBox();
+            this.burglar1 = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.policeOfficer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.burglar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordinaryMan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.burglar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.Color.Navy;
+            this.lblScore.Location = new System.Drawing.Point(3, 604);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(70, 20);
+            this.lblScore.TabIndex = 7;
+            this.lblScore.Text = "Points: 0";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLevel.ForeColor = System.Drawing.Color.Navy;
+            this.lblLevel.Location = new System.Drawing.Point(325, 604);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(63, 20);
+            this.lblLevel.TabIndex = 8;
+            this.lblLevel.Text = "Level: 1";
+            // 
+            // gameMessage
+            // 
+            this.gameMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameMessage.ForeColor = System.Drawing.Color.Navy;
+            this.gameMessage.Location = new System.Drawing.Point(-7, 249);
+            this.gameMessage.Name = "gameMessage";
+            this.gameMessage.Size = new System.Drawing.Size(412, 115);
+            this.gameMessage.TabIndex = 9;
+            this.gameMessage.Text = "LEVEL UP";
+            this.gameMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // btnPlayGame
+            // 
+            this.btnPlayGame.BackColor = System.Drawing.SystemColors.Window;
+            this.btnPlayGame.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlayGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayGame.ForeColor = System.Drawing.Color.Firebrick;
+            this.btnPlayGame.Location = new System.Drawing.Point(144, 133);
+            this.btnPlayGame.Name = "btnPlayGame";
+            this.btnPlayGame.Padding = new System.Windows.Forms.Padding(5);
+            this.btnPlayGame.Size = new System.Drawing.Size(116, 38);
+            this.btnPlayGame.TabIndex = 10;
+            this.btnPlayGame.Text = "Play Game";
+            this.btnPlayGame.UseVisualStyleBackColor = false;
+            this.btnPlayGame.Click += new System.EventHandler(this.btnPlayGame_Click);
+            // 
+            // pictureBoxCar
+            // 
+            this.pictureBoxCar.Image = global::GameApplication.Properties.Resources.police_car_76;
+            this.pictureBoxCar.Location = new System.Drawing.Point(-9, -1);
+            this.pictureBoxCar.Name = "pictureBoxCar";
+            this.pictureBoxCar.Size = new System.Drawing.Size(414, 637);
+            this.pictureBoxCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxCar.TabIndex = 11;
+            this.pictureBoxCar.TabStop = false;
+            // 
+            // bullet
+            // 
+            this.bullet.Image = global::GameApplication.Properties.Resources.bulletImage__1_;
+            this.bullet.Location = new System.Drawing.Point(181, 446);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(22, 32);
+            this.bullet.TabIndex = 5;
+            this.bullet.TabStop = false;
+            // 
+            // policeOfficer
+            // 
+            this.policeOfficer.Image = global::GameApplication.Properties.Resources.pic41;
+            this.policeOfficer.Location = new System.Drawing.Point(144, 497);
+            this.policeOfficer.Name = "policeOfficer";
+            this.policeOfficer.Size = new System.Drawing.Size(89, 100);
+            this.policeOfficer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.policeOfficer.TabIndex = 4;
+            this.policeOfficer.TabStop = false;
+            // 
+            // burglar2
+            // 
+            this.burglar2.Image = global::GameApplication.Properties.Resources.pic11;
+            this.burglar2.Location = new System.Drawing.Point(164, 23);
+            this.burglar2.Name = "burglar2";
+            this.burglar2.Size = new System.Drawing.Size(80, 80);
+            this.burglar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.burglar2.TabIndex = 2;
+            this.burglar2.TabStop = false;
+            // 
+            // ordinaryMan
+            // 
+            this.ordinaryMan.Image = global::GameApplication.Properties.Resources._764584__1_;
+            this.ordinaryMan.Location = new System.Drawing.Point(318, 23);
+            this.ordinaryMan.Name = "ordinaryMan";
+            this.ordinaryMan.Size = new System.Drawing.Size(46, 100);
+            this.ordinaryMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.ordinaryMan.TabIndex = 1;
+            this.ordinaryMan.TabStop = false;
+            this.ordinaryMan.Click += new System.EventHandler(this.ordinaryMan_Click);
+            // 
+            // burglar1
+            // 
+            this.burglar1.Image = global::GameApplication.Properties.Resources.PngItem_91940__1_;
+            this.burglar1.Location = new System.Drawing.Point(33, 12);
+            this.burglar1.Name = "burglar1";
+            this.burglar1.Size = new System.Drawing.Size(61, 100);
+            this.burglar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.burglar1.TabIndex = 0;
+            this.burglar1.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.White;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Firebrick;
+            this.lblTitle.Location = new System.Drawing.Point(55, 38);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(300, 65);
+            this.lblTitle.TabIndex = 12;
+            this.lblTitle.Text = "Cops VS Robbers";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(407, 637);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(398, 632);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnPlayGame);
+            this.Controls.Add(this.pictureBoxCar);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.bullet);
+            this.Controls.Add(this.policeOfficer);
+            this.Controls.Add(this.burglar2);
+            this.Controls.Add(this.ordinaryMan);
+            this.Controls.Add(this.burglar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.gameMessage);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "CopsVsRobbers";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.policeOfficer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.burglar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordinaryMan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.burglar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox burglar1;
+        private System.Windows.Forms.PictureBox ordinaryMan;
+        private System.Windows.Forms.PictureBox burglar2;
+        private System.Windows.Forms.PictureBox policeOfficer;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label gameMessage;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Button btnPlayGame;
+        private System.Windows.Forms.PictureBox pictureBoxCar;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GameApplication/GameApplication/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameApplication
+{
+    public partial class Form1 : Form
+    {
+        bool goRight;
+        bool goLeft;
+        bool shoot;
+        bool gameOver;
+        bool ordinaryManHit;
+        int score;
+        int level;
+        int scoreForLevelMsg;
+        int speedOfPoliceman;
+        int speedOfBurglar;
+        int bulletSpeed;
+        Random random;
+
+        public Form1()
+        {
+            this.random = new Random();
+            this.speedOfPoliceman = 15;
+            this.level = 1;
+            this.scoreForLevelMsg = 0;
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // If left key is pressed, the policeman moves to the left.
+            // If right key is pressed, the policeman moves to the right.
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+                e.SuppressKeyPress = true;
+
+            }
+            e.SuppressKeyPress = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // When left key is released, policeman should stop moving to the left. 
+            // Similarly, when right key is released. policeman should stop moving to the right.
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Space && shoot == false)
+            {
+                shoot = true;
+                bullet.Top = policeOfficer.Top - 32;
+                bullet.Left = policeOfficer.Left + (policeOfficer.Width / 2);
+            }
+            if (e.KeyCode == Keys.Enter && gameOver)
+            {
+                resetGame();
+            }
+        }
+
+        private void resetGame()
+        {
+            gameTimer.Start();
+            speedOfBurglar = 3;
+
+            burglar1.Left = random.Next(0, 300);
+            burglar2.Left = random.Next(0, 300);
+            ordinaryMan.Left = random.Next(0, 300);
+
+            burglar1.Top = random.Next(0, 200) * -1;
+            burglar2.Top = random.Next(0, 500) * -1;
+            ordinaryMan.Top = random.Next(0, 900) * -1;
+
+            score = 0;
+            level = 1;
+            bulletSpeed = 0;
+            bullet.Left = -300; // moving the bullet out of the form, it only needs to be seen when we press space button.
+            gameMessage.Visible = false;
+            ordinaryManHit = false;
+            shoot = false;
+            updateScore();
+            updateLevel();
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            burglar1.Top += speedOfBurglar;
+            burglar2.Top += speedOfBurglar;
+            ordinaryMan.Top += speedOfBurglar;
+
+            if (burglar1.Top > 559 || burglar2.Top > 559)
+            {
+                gameFinished();
+            }
+
+            if (ordinaryMan.Top > 620)
+            {
+                ordinaryMan.Top -= 900;
+                ordinaryMan.Left = random.Next(0, 300);
+            }
+
+            // With policeOfficer.Left we make sure the policeOfficer stays in the form.
+            if (goLeft && policeOfficer.Left > 0)
+            {
+                policeOfficer.Left -= speedOfPoliceman;
+            }
+            if (goRight && policeOfficer.Left < 297)
+            {
+                policeOfficer.Left += speedOfPoliceman;
+            }
+
+            // Making the bullet shoot a burglar.
+            if (shoot)
+            {
+                bulletSpeed = 15;
+                bullet.Top -= bulletSpeed;
+            }
+            else
+            {
+                bullet.Left = -300;
+                bulletSpeed = 0;
+            }
+
+            if (bullet.Top < -32)
+            {
+                shoot = false;
+            }
+
+            if (bullet.Bounds.IntersectsWith(burglar1.Bounds))
+            {
+                score += 1;
+                burglar1.Top = -500;
+                burglar1.Left = random.Next(0, 300);
+                shoot = false;
+                updateScore();
+            }
+
+            if (bullet.Bounds.IntersectsWith(burglar2.Bounds))
+            {
+                score += 1;
+                burglar2.Top = -700;
+                burglar2.Left = random.Next(0, 300);
+                shoot = false;
+                updateScore();
+            }
+
+            if (bullet.Bounds.IntersectsWith(ordinaryMan.Bounds))
+            {
+                ordinaryManHit = true;
+                gameFinished();
+            }
+
+            if (burglar1.Bounds.IntersectsWith(gameMessage.Bounds) && gameMessage.Text=="Level up" || 
+                burglar2.Bounds.IntersectsWith(gameMessage.Bounds) && gameMessage.Text == "Level up" || 
+                ordinaryMan.Bounds.IntersectsWith(gameMessage.Bounds) && gameMessage.Text == "Level up")
+            {
+                gameMessage.Visible = false;
+            }
+
+
+            if (burglar1.Bounds.IntersectsWith(burglar2.Bounds) ||
+                    burglar1.Bounds.IntersectsWith(ordinaryMan.Bounds))
+            {
+                burglar1.Top -= 100;
+            }
+
+            if (burglar2.Bounds.IntersectsWith(ordinaryMan.Bounds))
+            {
+                burglar2.Top -= 220;
+            }
+        }
+
+
+        private void updateScore()
+        {
+            lblScore.Text = string.Format("Points: {0}", score.ToString());
+            if(score == scoreForLevelMsg + 5)
+            {
+                gameMessage.Text = "Level up";
+                gameMessage.Visible = true;
+                gameMessage.BringToFront();
+                level += 1;
+                speedOfBurglar += 1;
+                scoreForLevelMsg += 5;
+                updateLevel();
+            }
+        }
+
+        public void updateLevel()
+        {
+            lblLevel.Text = string.Format("Level: {0}", level);
+        }
+
+        private void gameFinished()
+        {
+            gameOver = true;
+            gameTimer.Stop();
+            if (ordinaryManHit)
+            {
+                gameMessage.Text = "GAME OVER" + Environment.NewLine + "You killed a civilian!" + Environment.NewLine + "Press Enter to try again.";
+            }
+            else
+            {
+                gameMessage.Text = "GAME OVER" + Environment.NewLine + "Press Enter to try again.";
+            }
+            gameMessage.BringToFront();
+            gameMessage.Visible = true;
+        }
+
+        private void btnPlayGame_Click(object sender, EventArgs e)
+        {
+            pictureBoxCar.Visible = false;
+            btnPlayGame.Visible = false;
+            lblTitle.Visible = false;
+            btnPlayGame.Enabled = false;
+            resetGame();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ordinaryMan_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
